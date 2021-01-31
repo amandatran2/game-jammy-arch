32891e88c0f360ac835488bb9e695f4915c6287a
James Guo <jamesg22@uw.edu>
2021-01-31 11:47:56 -0800

    Destruction 1.0

---
+++ Assets/Scripts/Boom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Boom : MonoBehaviour
+{
+    public float radius = 5.0F;
+    public float power = 100.0F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 explosionPos = transform.position;
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+                if (rb != null)
+                    rb.isKinematic = false;
+                    rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnCubes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCubes : MonoBehaviour
+{
+    public int len = 50;
+    public int wid = 50;
+    public int hgt = 50;
+    public GameObject cube;
+    public Vector3 startpoint = new Vector3(0, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        for(int i =0; i<len; i++)
+        {
+            for (int j = 0; j < wid; j++)
+            {
+                for (int k = 0; k < hgt; k++)
+                {
+                    Instantiate(cube, new Vector3(startpoint.x+i, startpoint.y+j, startpoint.z+k), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+
+}
